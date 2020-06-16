e9db53e0144de7b5d5441e9b65c95eec4966a063
SE5910GroupProject <SE5910GroupProject@gmail.com>
2020-06-15 23:36:31 -0500

    Attempt 1

---
+++ retail-application/src/test/java/com/ucmo/groupproject/SE5910/retailapplication/RetailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucmo.groupproject.SE5910.retailapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
